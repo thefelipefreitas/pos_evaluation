a418f3ee3104f20fb50a105bdb23432a90c22726
Felipe Freitas <felipe.freitas2903@gmail.com>
2025-03-16 23:12:54 -0300

    creating the products files in the application layer

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Products/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,43 @@
+﻿using Ambev.DeveloperEvaluation.Common.Validation;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Products.CreateProduct;
+
+/// <summary>
+/// Command for creating a new product.
+/// </summary>
+/// <remarks>
+/// This command is used to capture the required data for creating a product, 
+/// including title, description, category, image and rating. 
+/// It implements <see cref="IRequest{TResponse}"/> to initiate the request 
+/// that returns a <see cref="CreateUserResult"/>.
+/// 
+/// The data provided in this command is validated using the 
+/// <see cref="CreateUserCommandValidator"/> which extends 
+/// <see cref="AbstractValidator{T}"/> to ensure that the fields are correctly 
+/// populated and follow the required rules.
+/// </remarks>
+public class CreateProductCommand : IRequest<CreateProductResult>
+{
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Category { get; set; } = string.Empty;
+
+    public string Image { get; set; } = string.Empty;
+
+    public Rating? Rating { get; set; }
+
+    public ValidationResultDetail Validate()
+    {
+        var validator = new CreateProductCommandValidator();
+        var result = validator.Validate(this);
+        return new ValidationResultDetail
+        {
+            IsValid = result.IsValid,
+            Errors = result.Errors.Select(o => (ValidationErrorDetail)o)
+        };
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Products/GetProduct/GetProductResult.cs
@@ -0,0 +1,44 @@
+using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Application.Products.GetProduct;
+
+/// <summary>
+/// Response model for GetProduct operation
+/// </summary>
+public class GetProductResult
+{
+    /// <summary>
+    /// The unique identifier of the product
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// The product's title
+    /// </summary>
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The product's description
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The product's price
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// The product's category
+    /// </summary>
+    public string Category { get; set; } = string.Empty;   
+
+    /// <summary>
+    /// The product's image
+    /// </summary>
+    public string Image { get; set; } = string.Empty;   
+
+    /// <summary>
+    /// The product's rating
+    /// </summary>
+    public Rating? Rating { get; set; }
+}
