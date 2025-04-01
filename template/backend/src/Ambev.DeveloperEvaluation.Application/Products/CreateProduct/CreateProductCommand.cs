275e75b564c40f915dec9b6636739a98e502f9cf
Felipe Freitas <felipe.freitas2903@gmail.com>
2025-04-01 20:29:47 -0300

    correting remarks

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Products/CreateProduct/CreateProductCommand.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Products/CreateProduct/CreateProductCommand.cs
@@ -10,10 +10,10 @@ namespace Ambev.DeveloperEvaluation.Application.Products.CreateProduct;
 /// This command is used to capture the required data for creating a product,
 /// including title, description, category, image and rating.
 /// It implements <see cref="IRequest{TResponse}"/> to initiate the request
-/// that returns a <see cref="CreateUserResult"/>.
+/// that returns a <see cref="CreateProductResult"/>.
 ///
 /// The data provided in this command is validated using the
-/// <see cref="CreateUserCommandValidator"/> which extends
+/// <see cref="CreateProductCommandValidator"/> which extends
 /// <see cref="AbstractValidator{T}"/> to ensure that the fields are correctly
 /// populated and follow the required rules.
 /// </remarks>
