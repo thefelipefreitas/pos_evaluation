74dc9c6909113e16ef2cd8417f24c395c0655b50
Felipe Freitas <felipe.freitas2903@gmail.com>
2025-04-05 23:22:57 -0300

    create new repository

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/ProductRepository.cs
@@ -0,0 +1,49 @@
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly DefaultContext _context;
+
+    public ProductRepository(DefaultContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Product>> GetAllAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    public async Task<Product?> GetByIdAsync(Guid id)
+    {
+        return await _context.Products.FirstOrDefaultAsync(s => s.Id == id);
+    }
+
+    public async Task<Product> AddAsync(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product> UpdateAsync(Product product)
+    {
+        _context.Products.Update(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product != null)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
