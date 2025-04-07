83d4d6a18e0b73d614d045b6b4a6573d9cb80266
Felipe Freitas <felipe.freitas2903@gmail.com>
2025-04-07 10:47:25 -0300

    creates the missing files for the product crud in the api

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Products/CreateProduct/CreateProductRequest.cs
@@ -0,0 +1,39 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Products.CreateProduct;
+
+/// <summary>
+/// Represents a request to create a new product in the system.
+/// </summary>
+public class CreateProductRequest
+{
+    /// <summary>
+    /// Gets or sets the title.
+    /// </summary>
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the description.
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the price.
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// Gets or sets the category.
+    /// </summary>
+    public string Category { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the image.
+    /// </summary>
+    public string Image { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the rating
+    /// </summary>
+    public required Rating Rating { get; set; }
+}
